7f7fb6f8c7da3fb78948deff53052af1c532d618
Himanshu Dubey <sorted-hd@Himanshus-MacBook-Pro.local>
2022-01-07 01:55:41 +0530

    Updated Responsive Nav

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function toggleMobileMenu(menu) {
   const aTag = document.getElementsByClassName("mobile-menu")[0];
   aTag.classList.add("anim-a");
 }
+
+function viewResumeHandler(resumebutton) {
+  const url =
+    "https://drive.google.com/file/d/17Y_nFNVfEdNeezX8W4ascx_nH23tln3x/view?usp=sharing";
+  window.open(url, "__blank").focus();
+}
