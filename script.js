5512ebb037d060b99765b90c1bdd80a6bae5efe7
sorted-hd <hdubey71@gmail.com>
2022-04-17 17:34:38 +0530

    Updated resume link

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ function toggleMobileMenu(menu) {
 
 function viewResumeHandler(resumebutton) {
   const url =
-    "https://drive.google.com/file/d/17Y_nFNVfEdNeezX8W4ascx_nH23tln3x/view?usp=sharing";
+    "https://drive.google.com/file/d/1uYAY2YKPmb5yZ1u3eNYsglZcj2HC6shB/view?usp=sharing";
   window.open(url, "__blank").focus();
 }
